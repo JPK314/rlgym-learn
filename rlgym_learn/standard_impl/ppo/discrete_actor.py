fix entropy calculation and logging

rlgym_learn/standard_impl/ppo/discrete_actor.py
@@ -64,10 +64,10 @@ class DiscreteFF(Actor[AgentID, np.ndarray, np.ndarray]):
     def get_backprop_data(self, agent_id_list, obs_list, acts, **kwargs):
         probs = self.get_output(obs_list)
         acts_tensor = torch.as_tensor(np.array(acts)).to(self.device)
-        action_probs = probs.gather(-1, acts_tensor)
-        action_logits = probs_to_logits(action_probs)
-        min_real = torch.finfo(action_logits.dtype).min
-        action_logits = torch.clamp(action_logits, min=min_real)
-        entropy = -(action_logits * action_probs).sum(dim=-1)
+        logits = probs_to_logits(probs)
+        min_real = torch.finfo(logits.dtype).min
+        logits = torch.clamp(logits, min=min_real)
+        entropy = -(logits * probs).sum(dim=-1)
+        action_logits = logits.gather(-1, acts_tensor)
 
         return action_logits.to(self.device), entropy.to(self.device).mean()

rlgym_learn/standard_impl/ppo/ppo_learner.py
@@ -205,12 +205,10 @@ class PPOLearner(
         """
 
         n_batches = 0
-        mean_clip = 0
-        mean_entropy = 0
-        mean_divergence = torch.tensor(
-            0, dtype=torch.float32, device=self.config.device
-        )
-        mean_val_loss = 0
+        tot_clip = 0
+        tot_entropy = 0
+        tot_divergence = 0
+        tot_val_loss = 0
 
         # Save parameters before computing any updates.
         actor_before = torch.nn.utils.parameters_to_vector(
@@ -287,7 +285,7 @@ class PPOLearner(
                             .item()
                             * minibatch_ratio
                         )
-                        mean_clip += clip_fraction
+                        tot_clip += clip_fraction
 
                     actor_loss = (
                         -torch.min(ratio * advantages, clipped * advantages).mean()
@@ -301,9 +299,9 @@ class PPOLearner(
                     ppo_loss.backward()
                     value_loss.backward()
 
-                    mean_val_loss += value_loss.cpu().detach().item()
-                    mean_divergence += kl
-                    mean_entropy += entropy.cpu().detach().item()
+                    tot_val_loss += value_loss.cpu().detach().item()
+                    tot_divergence += kl.cpu().detach().item()
+                    tot_entropy += entropy.cpu().detach().item()
 
                 torch.nn.utils.clip_grad_norm_(self.critic.parameters(), max_norm=0.5)
                 torch.nn.utils.clip_grad_norm_(self.actor.parameters(), max_norm=0.5)
@@ -326,14 +324,18 @@ class PPOLearner(
 
         self.cumulative_model_updates += n_batches
 
-        # If there were no batches, we just want to log the total time spent here, so just set n_batches to 1
+        # If there were no batches, we just want to log the total time spent here (and totals will all be 0 anyway), so just set n_batches to 1
         if n_batches == 0:
             n_batches = 1
+        mean_clip = tot_clip / n_batches
+        mean_entropy = tot_entropy / n_batches
+        mean_divergence = tot_divergence / n_batches
+        mean_val_loss = tot_val_loss / n_batches
         return PPOData(
             (time.time() - t1) / n_batches,
             self.cumulative_model_updates,
             mean_entropy,
-            mean_divergence.cpu().item(),
+            mean_divergence,
             mean_val_loss,
             mean_clip,
             actor_update_magnitude,

speed_test.py
@@ -177,9 +177,9 @@ if __name__ == "__main__":
             standardize_returns=True
         ),
     )
-    # wandb_config = WandbMetricsLoggerConfigModel(
-    #     group="rlgym-learn-testing", resume=True
-    # )
+    wandb_config = WandbMetricsLoggerConfigModel(
+        group="rlgym-learn-testing", resume=True
+    )
     ppo_agent_controller_config = PPOAgentControllerConfigModel(
         timesteps_per_iteration=50_000,
         save_every_ts=600_000,
@@ -191,7 +191,7 @@ if __name__ == "__main__":
         log_to_wandb=False,
         learner_config=learner_config,
         experience_buffer_config=experience_buffer_config,
-        # metrics_logger_config=wandb_config,
+        metrics_logger_config=wandb_config,
     )
 
     generate_config(
@@ -209,11 +209,8 @@ if __name__ == "__main__":
                     action_space_serde_type=PyAnySerdeType.TUPLE(
                         (PyAnySerdeType.STRING(), PyAnySerdeType.INT())
                     ),
-                    state_metrics_serde_type=PyAnySerdeType.LIST(
-                        PyAnySerdeType.NUMPY(np.float64)
-                    ),
                 ),
-                timestep_limit=500_000,
+                timestep_limit=500_000_000,
                 send_state_to_agent_controllers=False,
             ),
             agent_controllers_config={"PPO1": ppo_agent_controller_config},
@@ -227,8 +224,8 @@ if __name__ == "__main__":
             actor_factory,
             critic_factory,
             NumpyExperienceBuffer(GAETrajectoryProcessor()),
-            # metrics_logger=WandbMetricsLogger(PPOMetricsLogger()),
-            metrics_logger=PPOMetricsLogger(),
+            metrics_logger=WandbMetricsLogger(PPOMetricsLogger()),
+            # metrics_logger=PPOMetricsLogger(),
         )
     }
 